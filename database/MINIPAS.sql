6b6498614211ca931f080dcb1c5c4cd0809ec5d3
Aksel Schmidt <aks@trifork.com>
2014-03-10 08:39:15 +0100

    initial sql

---
+++ database/MINIPAS.sql
@@ -0,0 +1,7 @@
+--db2
+-- T_MINIPAS_SYNC:
+-- ID bigint identity/AUTO_INCREMENT
+-- IDNUMMER VARCHAR(255) // IDNUMMER from T_ADM20XX
+-- SKEMAOPDAT Date of latest creation/update of the IDNUMMER row
+-- SOURCE_TABLE_NAME VARCHAR(255) // e.g. T_ADM2013
+
